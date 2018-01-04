438e73e6f298a6f1d7dbbacedab4f0dfbf6d4ada
tivrama <joelschoolniknarvar>
2018-01-03 18:36:41 -0800

    Third Commit -W

--- viewControl.js
+++ viewControl.js
@@ -2,8 +2,18 @@
 
 
 $(document).ready(function(){
-    $("button").click(function(){
+    $("button.formSubmit").click(function(){
         $.get('/api/test', function(data, status){
+            alert(data);
+        });
+    });
+});
+
+
+$(document).ready(function(){
+	
+    $("button.secondCall").click(function(){
+        $.post('/api/getorder', function(data, status){
             alert(data);
         });
     });
